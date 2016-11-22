7791cbcf5d3fd4722b72ae827055c85906970770
Dheeraj <dheeraj333nyk@gmail.com>
2016-11-22 00:00:17 +0000

    add mongolab database

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ var barslistRoutes = require("./routes/barslist.js"),
     indexRoutes = require("./routes/index.js");
     
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost/bars_list");
+//mongoose.connect("mongodb://localhost/bars_list");
+mongoose.connect("mongodb://dheeraj:password@ds159517.mlab.com:59517/bestbars");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
